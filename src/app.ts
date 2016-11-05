f775995d9c159e25f1ec5ecd52658072894b1bf3
Caio Vaccaro <caiovaccaromora@gmail.com>
2016-11-05 12:43:51 -0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,17 @@
-import { makeDOMDriver } from '@cycle/dom';
+import { Stream } from 'xstream';
+import { div, h1, makeDOMDriver } from '@cycle/dom';
 import { run } from '@cycle/xstream-run';
 
+// If everything is a stream
+// how can you render this to the DOM?
+// div('#root', [
+//   h1('', 'Test')
+// ])
+
 function main() {
-  return { dom: '' };
+  return {
+    dom: ''
+  };
 }
 
 run(main, {
