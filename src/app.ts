0bbd2ccefc17c0cbb5137ab447fbac654f400ddb
caiovaccaro <cavaccaro@hugeinc.com>
2016-11-05 12:32:16 -0200

    2

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,8 @@
-import { Stream } from 'xstream';
-import { div, h1, makeDOMDriver } from '@cycle/dom';
+import { makeDOMDriver } from '@cycle/dom';
 import { run } from '@cycle/xstream-run';
 
-function main(sources) {
-  return {
-    dom: Stream.of(1)
-      .map(() =>
-        div('#root', [
-          h1('', 'Test')
-        ])
-      )
-  };
+function main() {
+  return { dom: '' };
 }
 
 run(main, {
