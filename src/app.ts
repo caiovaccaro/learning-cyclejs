35d8b5d1a966a5e425ec362b8a402e17085495e1
Caio Vaccaro <caiovaccaromora@gmail.com>
2016-11-05 12:47:06 -0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import { Stream } from 'xstream';
 import { input, div, h1, makeDOMDriver } from '@cycle/dom';
 import { run } from '@cycle/xstream-run';
 
+// Check cycles docs, the main function can receive "sources"
+// How can we make the h1 content to be the inputs value?
+// PS: Doesn't Stream.of(1) looks weird?
+
 function main(sources) {
   return {
     dom: Stream.of(1)
