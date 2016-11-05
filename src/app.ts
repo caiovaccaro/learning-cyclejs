31eb0c2ccd2b28880b34fe705e20db68c0d80e2d
caiovaccaro <cavaccaro@hugeinc.com>
2016-11-05 12:33:56 -0200

    1

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,3 @@
-import { Stream } from 'xstream';
-import { div, label, input, hr, h1, makeDOMDriver, VNode } from '@cycle/dom';
-import { DOMSource } from '@cycle/dom/xstream-typings';
-import { run } from '@cycle/xstream-run';
+function main() {}
 
-interface ISources {
-  dom: DOMSource;
-}
-
-interface ISinks {
-  dom: Stream<VNode>;
-}
-
-function main(sources: ISources): ISinks {
-  const sinks: ISinks = {
-    dom: Stream.of(1)
-      .map(() =>
-        div('#root', [
-          div('.wrap.container-fluid', [
-            div('.row.item-container', [
-              h1('', 'Test')
-            ])
-          ])
-        ])
-      )
-  };
-
-  return sinks;
-}
-
-run(main, {
-  dom: makeDOMDriver('#app')
-})
+run(main, { dom: '' })
