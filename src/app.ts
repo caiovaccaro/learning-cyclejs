797a19f5edc0d92d9bb505c13052e883431471f7
caiovaccaro <cavaccaro@hugeinc.com>
2016-11-05 13:14:20 -0200

    4-a

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,54 @@
 import { Stream } from 'xstream';
-import { input, div, h1, makeDOMDriver } from '@cycle/dom';
+import { button, input, div, h1, makeDOMDriver } from '@cycle/dom';
 import { run } from '@cycle/xstream-run';
 
 function main(sources) {
+  const inputEvent$ = sources.dom.select('.field')
+    .events('input')
+    .map(ev => ev.target.value)
+    .startWith('Test');
+
+  const buttonEvent$ = sources.dom.select('.inc')
+    .events('click')
+    .mapTo(+1);
+
+  const count$ = buttonEvent$.fold((acc, x) => acc + x, 0)
+
+  const events$ = Stream.combine(
+    inputEvent$,
+    count$
+  ).map(([text, number]) => {
+    return { text, number }
+  });
+
   return {
-    dom: sources.dom.select('.field').events('input')
-      .map(ev => ev.target.value)
-      .startWith('Test')
-      .map(value =>
+    dom: events$
+      .map(({text, number}) =>
         div('#root', [
-          h1('', value),
-          input('.field', {attrs: {type: 'text'}})
+          renderTitle(text, number),
+          renderInput(),
+          renderButton()
         ])
       )
   };
+
+  function renderTitle(text, number) {
+    return div([
+      h1('', text + ' ' + number)
+    ])
+  }
+
+  function renderInput() {
+    return div([
+      input('.field', {attrs: {type: 'text'}})
+    ])
+  }
+
+  function renderButton() {
+    return div([
+      button('.inc', 'Increment')
+    ])
+  }
 }
 
 run(main, {
